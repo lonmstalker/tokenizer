9827d6b514c77dd858a5e5e039e2b1897b928204
Никита Кочнев <nikitakocnev@MacBook-Air-Nikita.local>
2023-03-30 21:55:33 +0300

    added token factory

--- src/main/kotlin/io/lonmstalker/tokenizer/lexer/TokenFactory.kt
+++ src/main/kotlin/io/lonmstalker/tokenizer/lexer/TokenFactory.kt
@@ -2,16 +2,35 @@ package io.lonmstalker.tokenizer.lexer
 
 import io.lonmstalker.tokenizer.enums.Keywords
 import io.lonmstalker.tokenizer.enums.Types
+import java.util.function.Supplier
 
 class TokenFactory {
 
-    fun findToken(token: Char, lexerContext: LexerContext): LexicalItem {
+    fun findToken(token: Char, lexerContext: LexerContext): LexicalItem =
+        OPERATION_MAP[token]
+            ?.get()
+            ?.apply { setIndex(this, lexerContext) }
+            ?: this.tokenNotSupported(token)
+
+    fun findToken(token: String, lexerContext: LexerContext): LexicalItem =
+        LEXICAL_MAP[token]
+            ?.get()
+            ?.apply { setIndex(this, lexerContext) }
+            ?: this.tokenNotSupported(token)
+
+    private fun setIndex(lexicalItem: LexicalItem, lexerContext: LexerContext) {
+        lexicalItem.setIndex(lexerContext.currentLine, lexerContext.startIndex, lexerContext.endIndex)
     }
 
-    fun findToken(token: String, lexerContext: LexerContext): LexicalItem {
-    }
+    private fun tokenNotSupported(token: Any): Nothing = error("token $token not known")
 
     companion object {
+        @JvmStatic
+        private val OPERATION_MAP = mutableMapOf<Char, Supplier<LexicalItem>>()
+
+        @JvmStatic
+        private val LEXICAL_MAP = mutableMapOf<String, Supplier<LexicalItem>>()
+
         @JvmStatic
         private val RESERVED_TOKENS = HashSet(Keywords.values().map { it.word } + Types.values().map { it.word })
     }
